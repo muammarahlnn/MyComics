993a838a5fe06bbd35808483b921c33b718e9739
muammarahlnn <21muammarahlan@gmail.com>
2021-04-20 03:24:35 +0800

    add RecyclerView

--- app/src/main/java/com/ardnn/recycleviewassignmet/MainActivity.kt
+++ app/src/main/java/com/ardnn/recycleviewassignmet/MainActivity.kt
@@ -2,10 +2,29 @@ package com.ardnn.recycleviewassignmet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rvComics: RecyclerView
+    private var listComics: ArrayList<Comic> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvComics = findViewById(R.id.rv_comics)
+        rvComics.setHasFixedSize(true)
+
+        listComics.addAll(ComicsData.listData)
+        showRecyclerList()
     }
+
+    private fun showRecyclerList() {
+        rvComics.layoutManager = LinearLayoutManager(this)
+        val comicsAdapter = ComicsAdapter(listComics)
+        rvComics.adapter = comicsAdapter
+    }
+
+
 }
