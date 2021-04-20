c8469f701ae4e0bbc853436e06bfc761774a6dae
muammarahlnn <21muammarahlan@gmail.com>
2021-04-21 01:32:17 +0800

    add animation on SplashActivity

--- app/src/main/java/com/ardnn/recycleviewassignmet/SplashActivity.kt
+++ app/src/main/java/com/ardnn/recycleviewassignmet/SplashActivity.kt
@@ -1,21 +1,50 @@
 package com.ardnn.recycleviewassignmet
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
 
 class SplashActivity : AppCompatActivity() {
+    // widgets
+    private lateinit var tvMyComics: TextView
+    private lateinit var tvSlogan: TextView
+    private lateinit var ivLogoComics: ImageView
+
+    // animations
+    private lateinit var animSplash: Animation
+    private lateinit var animBottomToTop: Animation
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // set timer for 1 second
+        // initialize widgets
+        tvMyComics = findViewById(R.id.tv_mycomics_splash)
+        tvSlogan = findViewById(R.id.tv_slogan_splash)
+        ivLogoComics = findViewById(R.id.iv_logo_comics_splash)
+
+        // load animations
+        animSplash = AnimationUtils.loadAnimation(this, R.anim.splash_logo)
+        animBottomToTop = AnimationUtils.loadAnimation(this, R.anim.bottom_to_top)
+
+        // run animation
+        tvMyComics.animation = animSplash
+        ivLogoComics.animation = animSplash
+        tvSlogan.animation = animBottomToTop
+
+        // set timer for 1.5 second
         Handler(Looper.getMainLooper()).postDelayed({
             val goToMain: Intent = Intent(this@SplashActivity, MainActivity::class.java)
             startActivity(goToMain)
             finish()
-        }, 1000)
+        }, 1500)
     }
 }
