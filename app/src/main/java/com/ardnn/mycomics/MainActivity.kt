c0acd138c451d3820326026da0dc16a479a8bf72
muammarahlnn <21muammarahlan@gmail.com>
2021-04-26 23:44:56 +0800

    make MainActivity use Fragment

--- app/src/main/java/com/ardnn/mycomics/MainActivity.kt
+++ app/src/main/java/com/ardnn/mycomics/MainActivity.kt
@@ -1,48 +1,52 @@
 package com.ardnn.mycomics
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var rvComics: RecyclerView
-    private var listComics: ArrayList<Comic> = arrayListOf()
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    private lateinit var bnvMain: BottomNavigationView
+    private lateinit var fragmentMap: MutableMap<Int, Fragment>
     private val datasetName: String = "dataset.txt"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get dataset comics from assets folder
+
+        // get dataset comics from assets folder and resources
         ComicsData.getDataset(this, datasetName)
 
+        // initialize widgets
+        bnvMain = findViewById(R.id.bnv_main)
+        fragmentMap = mutableMapOf<Int, Fragment>()
 
-        // initialize widget
-        rvComics = findViewById(R.id.rv_comics_main)
-        rvComics.setHasFixedSize(true)
-
-        listComics.addAll(ComicsData.listData)
-        showRecyclerList()
     }
 
-    private fun showRecyclerList() {
-        rvComics.layoutManager = LinearLayoutManager(this)
-        val comicsAdapter = ComicsAdapter(listComics)
-        rvComics.adapter = comicsAdapter
+    override fun onStart() {
+        super.onStart()
 
-        comicsAdapter.setOnItemClickCallback(object : ComicsAdapter.OnItemClickCallback {
-            override fun onItemClicked(data: Comic) {
-                showSelectedComic(data)
-            }
-        })
+        fragmentMap.put(R.id.menu_item_home, HomeFragment.newInstance())
+        fragmentMap.put(R.id.menu_item_recent, RecentFragment.newInstance())
+        fragmentMap.put(R.id.menu_item_favorite, FavoriteFragment.newInstance())
+
+        bnvMain.setOnNavigationItemSelectedListener(this)
+        bnvMain.selectedItemId = R.id.menu_item_home
     }
 
-    private fun showSelectedComic(comic: Comic) {
-        var goToDetail: Intent = Intent(this@MainActivity, DetailActivity::class.java)
-        goToDetail.putExtra(DetailActivity.EXTRA_COMICS, comic)
-        startActivity(goToDetail)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        val fragment = fragmentMap.get(item.itemId)
+        if (fragment != null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.fl_main, fragment)
+                    .commit()
+        }
+        return true
     }
 
 }
